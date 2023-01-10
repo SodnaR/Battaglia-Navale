929ab04aa3fb24654d68cc66efe39023e244918d
MattiaSrt <118407444+MattiaSrt@users.noreply.github.com>
2023-01-10 15:55:37 +0100

    rotation, get rotation, set armor
    rinominato "orient" con "rotation"
    aggiunti metodi "getRotation" e "setArmor"

--- ShipsMatt/Ship.h
+++ ShipsMatt/Ship.h
@@ -7,14 +7,16 @@ class Ship{
 		int armor;
 		bool alive=true;
 		std::vector<int> center(2);
-		int orien; //orientazione nave 0=orizzontale, 1 verticale
+		int rotation; //orientazione nave 0=orizzontale, 1 verticale
 	public:
 		int getArmor(){return armor;}
+		int getRotation(){ return rotation;}
 		bool isAlive(){return alive;}
 		virtual void move()=0;
 		void locateCenter();
 		void hit(){armor=armor-1;}
 		void sink(){if(armor==0) alive=false;}
+		void setArmor(int a);
 		class Invalid{};
 };
 
